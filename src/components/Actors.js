bfdb407fd578938d4bdc2b1735c67690c604284d
RoUchiha <swaggsikh@gmail.com>
2022-05-05 21:32:55 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+
+      {actors.map(m => 
+    <div>
+      <h1>{m.name}</h1>
+      
+      <ul>
+        {m.movies.map(gen => 
+          <li>
+            {gen}
+          </li>)}
+      </ul>
+    </div>)}
     </div>
   );
 };
