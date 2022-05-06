f0ac79a4d238d305aacbf999134df98e78310927
RoUchiha <swaggsikh@gmail.com>
2022-05-05 21:32:33 -0500

    commit tree ca45c7b643884e6420a5eb998fdb84df33c5ab17

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+
+
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        
+        {movies.map(m => 
+    <div>
+      <h1>{m.title}</h1>
+      <h3>{m.time} mins</h3>
+      <ul>
+        {m.genres.map(gen => 
+          <li>
+            {gen}
+          </li>)}
+      </ul>
+    </div>)}
     </div>
   );
 };
